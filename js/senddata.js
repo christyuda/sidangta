57cddefcbd01dec75c630158deccdc577e503b8d
christyuda <yudacihuy8@gmail.com>
2023-08-09 17:08:30 +0700

    update modul

---
+++ js/senddata.js
@@ -0,0 +1,7 @@
+const judul = document.getElementById('judul').value;
+const pembimbing1 = document.getElementById('pembimbing1').value;
+const pembimbing2 = document.getElementById('pembimbing2').value;
+const penguji = document.getElementById('penguji').value;
+const penguji2 = document.getElementById('penguji2').value;
+
+const token = "";
